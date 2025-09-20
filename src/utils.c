59ef9a32dc803aa031b07e87460d3fbdb9211e82
zouazrou <183734171+zouazrou@users.noreply.github.com>
2025-09-20 18:39:42 +0100

    Fix Normette

--- src/utils.c
+++ src/utils.c
@@ -6,41 +6,41 @@
 /*   By: zouazrou <zouazrou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 08:22:36 by zouazrou          #+#    #+#             */
-/*   Updated: 2025/09/20 18:32:41 by zouazrou         ###   ########.fr       */
+/*   Updated: 2025/09/20 18:38:25 by zouazrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-bool    is_outside_window(int x, int y)
+bool	is_outside_window(int x, int y)
 {
-    t_game *g;
+	t_game	*g;
 
-    g = get_addr_t_game(NULL);
-    if (x < 0 || x >= g->width || y < 0 || y >= g->height)
-        return (true);
-    return (false);
+	g = get_addr_t_game(NULL);
+	if (x < 0 || x >= g->width || y < 0 || y >= g->height)
+		return (true);
+	return (false);
 }
 
-void put_pixel_in_image(int x, int y, int col)
+void	put_pixel_in_image(int x, int y, int col)
 {
-    int     offset;
-    t_game *g;
-    
-    g = get_addr_t_game(NULL);
+	int		offset;
+	t_game	*g;
+
+	g = get_addr_t_game(NULL);
 	if (is_outside_window(x, y))
-        return ;
-    offset = (y * g->img.line) + (x * (g->img.bpp / 8));
-    *(unsigned int *)(g->img.pixels + offset) = col;
+		return ;
+	offset = (y * g->img.line) + (x * (g->img.bpp / 8));
+	*(unsigned int *)(g->img.pixels + offset) = col;
 }
 
-t_game *get_addr_t_game(t_game *ptr)
+t_game	*get_addr_t_game(t_game *ptr)
 {
-    static t_game *addr = NULL;
+	static t_game	*addr = NULL;
 
-    if (ptr)
-        addr = ptr;
-    return (addr);
+	if (ptr)
+		addr = ptr;
+	return (addr);
 }
 
 void	init_ray_struct(t_ray *ray, double ray_angle)
