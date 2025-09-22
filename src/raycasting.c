2c312aaff7c0f8dadbd022ef60371ec592ebf105
Mohamed Elayyady <simo.contactt@gmail.com>
2025-09-22 15:54:13 +0100

    Fix mini fish eye

--- src/raycasting.c
+++ src/raycasting.c
@@ -32,18 +32,21 @@ void	ray_casting(t_game *g)
 	t_ray	ray_h;
 	t_ray	ray_v;
 	double	ray_angle;
-	double	ray_inc;
+	// double	ray_inc;
 	int		i;
 
-	ray_angle = g->ply.angle - (g->fov / 2);
-	ray_inc = g->fov / g->num_rays;
+	// ray_angle = g->ply.angle + atan((i - (g->num_rays * 0.5f)) / g->distance_to_plane);
+
+	// ray_angle = g->ply.angle - (g->fov / 2);
+	// ray_inc = g->fov / g->num_rays;
 	i = -1;
 	while (++i < g->num_rays)
 	{
+	    ray_angle = g->ply.angle + atan((i - (g->num_rays * 0.5f)) / g->distance_to_plane);
 		ray_h = horizontal_hit(normalize_angle(ray_angle));
 		ray_v = vertical_hit(normalize_angle(ray_angle));
 		choose_nearest(g->ray + i, &ray_h, &ray_v);
 		draw_3d_view(i);
-		ray_angle += ray_inc;
+		// ray_angle += ray_inc;
 	}
 }
